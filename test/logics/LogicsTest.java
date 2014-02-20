053822576c7ba7b72b2a97e748b7395a08f253fd
kasunsk <kasunsk@gmail.com>
2014-02-20 22:01:20 +0530

    Testing added succesfully

--- test/logics/LogicsTest.java
+++ test/logics/LogicsTest.java
@@ -1,10 +1,11 @@
+package logics;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 
-import logics.Logics;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
